6410de82532ba368760d4ffaf91df43caf241543
Leonid Kostyuchenko <kstchnko@gmail.com>
2021-02-14 20:40:33 +0300

    work on bugs

--- src/components/IconSwitch.jsx
+++ src/components/IconSwitch.jsx
@@ -3,13 +3,9 @@ import { IconSwitchPropTypes } from './PropTypes';
 export default function IconSwitch(props) {
   const { icon, onSwitch } = props;
 
-  function switchHandler(click) {
-    onSwitch(click.target.dataset.icon);
-  }
-
   return (
     <div className="icon-wrapper">
-      <span data-icon={icon} className="material-icons" onClick={switchHandler}>
+      <span data-icon={icon} className="material-icons" onClick={() => onSwitch(icon)}>
         {icon}
       </span>
     </div>
